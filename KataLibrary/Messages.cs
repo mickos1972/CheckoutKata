e790258c79439b051df994700a1d484d85cb2cb6
mick <mickm15@hotmail.com>
2020-10-23 12:07:07 +0100

    Added a bit more messaging

---
+++ KataLibrary/Messages.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace KataLibrary
+{
+    public static class StandardMessages
+    {
+        public static void PleaseEnterSelectionText()
+        { 
+            Console.WriteLine("Please select an item to add to the basket (A,B,C,D) press x to calculate total");
+        }
+
+        public static void TotalMessage(decimal total)
+        {
+            Console.WriteLine($"The Total is : £{total}");
+        }
+
+        public static void YouHaveAdded(ItemModel item)
+        {
+            Console.WriteLine($"You have added : {item.ItemName} costing {item.UnitPrice}");
+        }
+    }
+}
